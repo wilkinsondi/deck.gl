0132a46d81be199852f964071f0d6073c6e3f165
Ib Green <ib@uber.com>
2018-04-24 08:14:47 -0700

    wip

--- modules/core/src/lib/pick-layers.js
+++ modules/core/src/lib/pick-layers.js
@@ -168,6 +168,8 @@ export function pickVisibleObjects(
     height: deviceBottom - deviceTop
   };
 
+  console.error(pickingFBO.width, pickingFBO.height, pixelRatio, deviceRect);
+
   const pickedColors = drawAndSamplePickingBuffer(gl, {
     layers,
     viewports,
